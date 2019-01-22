2031c6b6d21963091a554bb6ebe430a41cde3a19
lucarge <luca.argenziano@icloud.com>
2019-01-22 09:04:24 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { List, Map } from 'immutable'
+import Immutable from 'immutable'
 
-interface AllowedList extends List<AllowedValue> {}
+interface AllowedList extends Immutable.List<AllowedValue> {}
 
-interface AllowedMap extends Map<string, AllowedValue> {}
+interface AllowedMap extends Immutable.Map<string, AllowedValue> {}
 
 type AllowedKey = string
 
@@ -21,16 +21,18 @@ type MapTypeAllowedData<DataType> = {
 
 type Remove<T, U> = T extends U ? never : T;
 
-// @ts-ignore
-interface TypedMap<DataType extends MapTypeAllowedData<DataType>> extends Map<AllowedKey, AllowedValue> {
+interface TypedMap<DataType
+extends MapTypeAllowedData<DataType>>
+extends Immutable.Map<AllowedKey, AllowedValue> {
   toJS(): DataType;
   get<K extends keyof DataType>(key: K, notSetValue?: AllowedValue): DataType[K];
   set<T extends string, V>(t: T, v: V): TypedMap<{ [P in Remove<keyof DataType, T>]: DataType[P] } & { [k in T]: V }>;
 }
 
-const createTypedMap = <DataType extends MapTypeAllowedData<DataType>>(data: DataType): TypedMap<DataType> => Map(data as any) as TypedMap<DataType>
+const Map = <DataType extends MapTypeAllowedData<DataType>>
+  (data: DataType): TypedMap<DataType> => Immutable.Map(data as any) as TypedMap<DataType>
 
-const basicMap = createTypedMap({
+const basicMap = Map({
   id: 'lucarge',
   username: 'lucarge',
   age: 24,
@@ -39,8 +41,17 @@ const basicMap = createTypedMap({
 
 const username = basicMap.get('id')
 
-const mapWithWallet = basicMap.set('wallet', createTypedMap({
+const mapWithWallet = basicMap.set('wallet', Map({
   credits: 50,
 }))
+
+// TODO: missing type inference with setIn/getIn
+const paymentMethod = mapWithWallet.setIn(['wallet', 'paymentMethod'], 'paypal').getIn(['wallet', 'paymentMethod'])
+
+const emptyMap = basicMap.clear()
+
+// TODO: after using `remove` membership inference is lost
+const mapWithoutUsername = basicMap.remove('username').get('id')
+const mapWithoutUsername2 = basicMap.delete('username').get('id')
 
 console.log(username, mapWithWallet)
